24c0659f1c291294ca34b7eb5b7642a014ab346c
Joshua Andes <andesjoshua@comcast.net>
2021-09-21 12:13:31 -0400

    commit tree 11cbdd338959abc5fc744c25c9080e2a1ace83eb

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase()
+    return customerName    
+}
+var setBestCustomer = () => {
+    bestCustomer = 'not bob';
+}
+var overwriteBestCustomer = () => {
+    return bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'mary';
+var changeLeastFavoriteCustomer = () => {
+    return leastFavoriteCustomer = 'cho'
+}
